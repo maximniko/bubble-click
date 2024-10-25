11852de67c4dbbca8ef34f2d93d5d0eae0460cbe
Maxim Niko <m.nikohdiuk@autodoc.eu>
2024-10-25 10:07:28 +0300

    touch handled

--- frontend/src/app/modules/lobster-coin/components/main/coin/coin.component.ts
+++ frontend/src/app/modules/lobster-coin/components/main/coin/coin.component.ts
@@ -10,7 +10,7 @@ import {ClickAnimationDirective} from './click-animation.directive';
   imports: [CommonModule, FormsModule, ClickAnimationDirective],
   styleUrl: 'coin.component.scss',
   template: `
-    <div class="clicker-container" (click)="onClick($event)" style="width:14rem;height:14rem;background-color:red;">
+    <div class="clicker-container" (touchend)="onClick($event)" style="width:14rem;height:14rem;background-color:red;">
       @for (click of clicks; track click.id) {
         <div class="click color-accent"
              [appClickAnimation]="click.id"
@@ -32,26 +32,29 @@ export class CoinComponent {
   }
 
   // Обрабатываем событие клика и передаем в Subject
-  onClick(event: MouseEvent) {
+  onClick(event: TouchEvent) {
     // Генерируем случайные значения для поворота
-    const rotation = Math.random() * 360 - 180; // Рандомное вращение от -180 до 180
 
-    // Создаем новый объект клика с координатами
-    const newClick = {
-      id: this.clickCounter++,
-      top: event.clientY,   // Координата Y клика
-      left: event.clientX,  // Координата X клика
-      rotation
-    };
+    for (let i = 0; i < event.changedTouches.length; i++) {
+      let touch: Touch = event.changedTouches[i]
+      let rotation: number = Math.random() * 360 - 180
+      // Создаем новый объект клика с координатами
+      const newClick = {
+        id: this.clickCounter++,
+        top: touch.clientY,   // Координата Y клика
+        left: touch.clientX,  // Координата X клика
+        rotation
+      };
 
-    this.clicks.push(newClick);
+      this.clicks.push(newClick);
 
-    this.coinsService.onClick()
+      this.coinsService.onClick()
 
-    // Удаляем элемент через 1.5 секунды после анимации
-    setTimeout(() => {
-      this.clicks = this.clicks.filter(click => click.id !== newClick.id);
-    }, 1500);
+      // Удаляем элемент через 1.5 секунды после анимации
+      setTimeout(() => {
+        this.clicks = this.clicks.filter(click => click.id !== newClick.id);
+      }, 1500);
+    }
   }
 }
 
