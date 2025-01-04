99368bdb4e5eb601f426bc9a32ff3ec8b26a21db
maxim.niko <m.nikohdiuk@autodoc.eu>
2025-01-04 14:13:04 +0200

    gh-pages

---
+++ docs/chunk-JXF7FSAE.js
@@ -0,0 +1,1 @@
+import{gc as n}from"./chunk-RFSBBNRU.js";function t(r){try{return JSON.parse(r)}catch{return}}function i(r){return n.production?`/bubble-click${r}`:r}export{t as a,i as b};
--- docs/chunk-JXRJSF23.js
+++
@@ -1,1 +0,0 @@
-import{fc as n}from"./chunk-KV6LOXJG.js";function t(r){try{return JSON.parse(r)}catch{return}}function i(r){return n.production?`/bubble-click${r}`:r}export{t as a,i as b};
